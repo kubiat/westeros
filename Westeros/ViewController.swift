d9a686376101b17f6ff6e294a9db9b3150eb55a6
Kubiat <kubiat@gmail.com>
2018-09-06 21:12:12 +0200

    Initial Commit

---
+++ Westeros/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Westeros
+//
+//  Created by Joaquín Gracia López on 6/9/18.
+//  Copyright © 2018 Web Cor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
